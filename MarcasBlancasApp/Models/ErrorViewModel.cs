e535bf49d3836d26c54df8687fd25618d4827989
Aaron <aaronhorna03@gmail.com>
2026-06-18 19:52:21 -0500

    creacion dotnet

---
+++ MarcasBlancasApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MarcasBlancasApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
